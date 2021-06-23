c6387dfccd201a3fa317b723e13beb5bce847fea
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-24 01:13:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def helloo():
     df=pd.read_csv("airtravel.csv",doublequote=False)
     return df.to_html()
 if__name__=="__main__":
