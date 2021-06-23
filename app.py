9c0feea62ffca2d19c087f6fb3d8ec6dedf6b9f5
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-23 17:02:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import pandas as pd
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    def = pd.read_csv("airtravel.csv")
-    return df.to_html()
-
-if __name__=="__main__":
-    app.run()
+    return "Hello, World!"
